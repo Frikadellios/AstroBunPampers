3d6c4d361e242d7dda0aab4993e6447576e285ee
Hrihorii Ilin <devopsick@pm.me>
2023-09-23 02:13:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AquaPump
+
+Aquapumpers
---
+++ prettier.config.cjs
@@ -0,0 +1,17 @@
+import dist from "@astrojs/react";
+
+/** @type {import("prettier").Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  pluginSearchDirs: [dist],
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
